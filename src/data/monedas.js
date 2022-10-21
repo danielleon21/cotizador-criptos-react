e4c9768667c76afb1818bba3ebbc1e10580bc668
Daniel Leon <danielmatus468@gmail.com>
2022-10-20 23:48:40 -0500

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dolar Estadounidense' },
+    { id: 'MXN', nombre: 'Peso Mexicano' },
+    { id: 'GBP', nombre: 'Libra Esterlina' },
+    { id: 'EUR', nombre: 'Euro ' }
+]
+
+export {
+    monedas
+}
